808796f1adde05c349f77d99e1a394c8579fdf31
Marcelhrb <marcelhrb@gmail.com>
2026-05-14 17:11:38 -0300

    feat: add status route with database connection info

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,25 @@
+import dotenv from "dotenv";
 import Fastify from "fastify";
+import { knex } from "../db/database.ts";
+
 export const app = Fastify();
 
 const message = "VAI CORINTHIANS!!";
-
 app.get("/", async function handler(request, reply) {
   return message;
 });
 
 app.get("/status", async () => {
-    return {
-        
-    }  
-})
+  const maxConnections = (await knex.raw("SHOW max_connections;")).rows[0]
+    .max_connections;
+
+  const opendConnection = await knex("pg_stat_activity")
+    .count("*")
+    .where("datname", process.env.POSTGRES_DB)
+    .first();
+
+  return {
+    Max_connections: maxConnections,
+    Opned_connection: opendConnection?.count,
+  };
+});
