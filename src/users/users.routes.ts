882a328d42cf8e0544243c9c6197786507f541af
Marcelhrb <marcelhrb@gmail.com>
2026-05-19 15:41:53 -0300

    feat: add login route with session management and cookie setup

--- src/users/users.routes.ts
+++ src/users/users.routes.ts
@@ -3,4 +3,5 @@ import UserController from "./users.controller.ts";
 
 export async function userRoutes(app: FastifyInstance) {
   app.post("/", UserController.create);
+  app.post("/login", UserController.login);
 }
