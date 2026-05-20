13cb7281c100a7e2362c67943e6b505c007d0eab
Marcelhrb <marcelhrb@gmail.com>
2026-05-19 21:57:41 -0300

    test: add PUT /meals/:id e2e test

--- src/meals/meals.route.ts
+++ src/meals/meals.route.ts
@@ -3,5 +3,5 @@ import MealController from "./meals.controller.ts";
 
 export async function mealRoutes(app: FastifyInstance) {
   app.post("/", MealController.create);
-  app.put(`/:id`, MealController.updateMeal)
+  app.put(`/:id`, MealController.updateMeal);
 }
